8ca578156e4c0e940db4e48c4842a6acc49b31ac
Robert Schumann <robertschumann@12MBP.local>
2019-08-14 18:21:33 -0400

    Networking

---
+++ networking/net-watcher.js
@@ -0,0 +1,24 @@
+'use strict'
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('Error: no file specified');
+}
+
+net.createServer(connection => {
+    // Reporting
+    console.log('Subscriber connected.');
+    connection.write(`Now watching ${filename} for changes.`);
+
+    // watcher setup
+    const watcher = 
+        fs.watch(filename, () => connection.write(`File changed ${new Date()}\n`));
+
+    // clean up
+    connection.on('close', () => {
+        console.log('Subscriber disconnected.');
+        watcher.close();
+    });
+}).listen(60300, () => console.log('Listening for subscribers.'));
