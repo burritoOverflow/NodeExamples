11e22f48cdde61033c82904bc5b4f875ff327ad9
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-08-26 16:50:12 -0400

    Update long-running-server-ex.js
    clearer comments

--- child-processes/long-running-server-ex.js
+++ child-processes/long-running-server-ex.js
@@ -4,7 +4,7 @@ const { fork } = require('child_process');
 const server = http.createServer();
 
 // note now that this allows the server to respond to multiple 
-// requests
+// requests, as a result of forking the long running process
 server.on('request', (req, res) => {
     if (req.url === '/compute') {
         // this inital approach will block the event loop 
