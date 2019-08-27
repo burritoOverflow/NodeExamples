f4eebfcfcb19b8257eae0b3323cea38be2754d3d
burritoOverflow <rschuman@ramapo.edu>
2019-08-27 17:45:21 -0400

    notes

--- event-driven-arch-example/async-callback.js
+++ event-driven-arch-example/async-callback.js
@@ -12,9 +12,11 @@ const readFileAsArray = function(file, callback) {
 }
 
 // example call
+// callback is passed as last argument to the host function
 readFileAsArray('./numbers.txt', (err, lines) => {
     if (err) throw err;
     
+    // parse the array to numbers
     const numbers = lines.map(Number);
     const oddNumbers = numbers.filter(number => number % 2 === 1);
     console.log(`Odd numbers count: ${oddNumbers.length}`);
