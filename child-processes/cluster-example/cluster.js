b3094fa89c6de099180adea804ed4ca466897555
burritoOverflow <rschuman@ramapo.edu>
2019-08-27 13:33:25 -0400

    IPC communication example

--- child-processes/cluster-example/cluster.js
+++ child-processes/cluster-example/cluster.js
@@ -1,13 +1,34 @@
 const os = require('os');
 const cluster = require('cluster');
 
+// mock function to return the number of users in a database
+const numUsersDb = () => {
+    this.count = this.count || 5;
+    this.count = this.count * this.count;
+    return this.count;
+}
+
 if (cluster.isMaster) {
     // fork for the number of cpu cores
     const cpus = os.cpus().length;
     console.log(`Forking for ${cpus}`);
+    
     for (let i = 0; i < cpus; i++) {
         cluster.fork();
     }
+    
+    // update the worker processes with the user count
+    const updateWorkers= () => {
+        const userCount = numUsersDb();
+        Object.values(cluster.workers).forEach(worker => {
+            worker.send({ userCount});
+        });
+    };
+
+    updateWorkers();
+    // update this value every 10 seconds--
+    // all workers recieve this value every 10 seconds over IPC channel
+    setInterval(updateWorkers, 10000);
 } else {
     // we'll use the example server here
     require('./single-server.js');
--- child-processes/cluster-example/single-server.js
+++ child-processes/cluster-example/single-server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const pid = process.pid;
 
+let userCount;
+
 http.createServer((req, res) => {
     // simulate a long running process
     for (let i = 0; i < 1e7; i++);
-    console.log(`Response handled by process ${pid}`);
-    res.end(`Response handled by process ${pid}`);
+    console.log(`Response handled by process:  ${pid}`);
+    res.write(`Response handled by process ${pid}\n`);
+    res.end(`Users: ${userCount}\n`);
 }).listen(8000, () => console.log(`Process ${pid} listening on port 8000`));
+
+// upon reciept of message, update the user count with this value
+process.on('message', msg => {
+    userCount = msg.userCount;
+})
