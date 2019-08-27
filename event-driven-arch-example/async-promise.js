e7f296900210844acbfba9614ce66901bbcbe2fc
burritoOverflow <rschuman@ramapo.edu>
2019-08-27 17:56:22 -0400

    promises and remaining call back interface

---
+++ event-driven-arch-example/async-promise.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+
+// note the callback calls are now replaced with promise calls
+// provide a default argument for the callback in case used with 
+// promise. Use a default empty argument in this example.
+const readFileAsArray = function(file, callback = () => {}) {
+    // this promise wraps the fs.readFile async call
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, function (err, data) {
+            if (err) {
+                reject(err);
+                return callback(err);
+            }
+
+            const lines = data.toString().trim().split('\n');
+            resolve(lines);
+            callback(null, lines);
+        });
+    });
+};
+
+// example call using promises
+// use the catch call to handle errors
+readFileAsArray('./numbers.txt')
+    .then(lines => {
+        const numbers = lines.map(Number);
+        const oddNumbers = numbers.filter(number => number % 2 === 1);
+        console.log(`Odd numbers count: ${oddNumbers.length}`);
+    })
+    .catch(console.error);
+
+// example call with callback
+// callback is passed as last argument to the host function
+readFileAsArray('./numbers.txt', (err, lines) => {
+    if (err) throw err;
+
+    // parse the array to numbers
+    const numbers = lines.map(Number);
+    const oddNumbers = numbers.filter(number => number % 2 === 1);
+    console.log(`Odd numbers count: ${oddNumbers.length}`);
+});
+
+console.log('Look, promises and callbacks!');
