a013a0e2ed5cf2c648ac2aecdd4e2ffe6a555ac9
Rob <rschumann88@gmail.com>
2019-08-28 15:53:03 -0400

    notes added

--- https_server/http-server.js
+++ https_server/http-server.js
@@ -1,6 +1,13 @@
 const http = require('http');
 
+// note that the request and response arguments are streams
+// req is a readable stream, res is a writable stream
 const requestListener = (req, res) => {
+  // display the first level of properties for the
+  // request object.
+  // note it's of type IncomingMessage
+  console.dir(req, { depth: 0});
+  console.dir(res, { depth: 0});
   res.end('Hello World');
 };
 
